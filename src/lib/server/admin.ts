3c2eafff787d8d9d3dd71d92f30fdf3570e80c85
TannerJ-Prompted <tanner@promptedweb.com>
2023-07-31 13:21:11 +1000

    Update admin.ts

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -6,9 +6,9 @@ import pkg from 'firebase-admin';
 try {
   pkg.initializeApp({
     credential: pkg.credential.cert({
-      projectId: FB_PROJECT_ID,
-      clientEmail: FB_CLIENT_EMAIL,
-      privateKey: FB_PRIVATE_KEY,
+      projectId: process.env.FB_PROJECT_ID,
+      clientEmail: process.env.FB_CLIENT_EMAIL,
+      privateKey: process.env.FB_PRIVATE_KEY,
     }),
   });
 } catch (err: any) {
