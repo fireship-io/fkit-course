9200ab0f36a062e8d62de95bc3b8bc71c7d0a221
TannerJ-Prompted <tanner@promptedweb.com>
2023-07-31 13:49:23 +1000

    Update admin.ts

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -8,7 +8,7 @@ try {
     credential: pkg.credential.cert({
       projectId: process.env.FB_PROJECT_ID,
       clientEmail: process.env.FB_CLIENT_EMAIL,
-      privateKey: JSON.parse(process.env.FB_PRIVATE_KEY),
+      privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
   });
 } catch (err: any) {
